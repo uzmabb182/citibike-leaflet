d13b8d3da3b2f49a4301f86ed50f2a0418cc0865
Mubashira Qari <81127634+uzmabb182@users.noreply.github.com>
2021-07-17 21:28:26 -0400

    Finish cicti map

--- static/js/logic.js
+++ static/js/logic.js
@@ -5,7 +5,7 @@
 url = "https://gbfs.citibikenyc.com/gbfs/en/station_information.json"
 
 d3.json(url).then(function (response) {
-  // Once we get a response, send the data.features object to the createFeatures function.
+  // Once we get a response, send to the function.
   console.log(response); 
   createMarkers(response);
 });
@@ -13,8 +13,7 @@ d3.json(url).then(function (response) {
 
 function createMarkers(response) {
 
-  // Define a function that we want to run once for each feature in the features array.
-  // Give each feature a popup that describes the place and time of the earthquake.
+  // Define a function that we want to run once for each station 
   
   let stations = response.data.stations;
   
@@ -24,28 +23,28 @@ function createMarkers(response) {
 
     // For each station, create a marker, and bind a popup with the station's name.
     let bikeMarker = L.marker([stations[i].lat, stations[i].lon])
-      .bindPopup("<h3>" + station.name + "<h3><h3>Capacity: " + station.capacity + "</h3>");
-
+      .bindPopup("<h3>" + stations.name + "<h3><h3>Capacity: " + stations.capacity + "</h3>");
+      
     // Add the marker to the bikeMarkers array.
     bikeStations.push(bikeMarker);
-    console.log(bikeStations)
+   
   }
  
   bikesLayer = L.layerGroup(bikeStations)
 
-  // Send our earthquakes layer to the createMap function/
+  // Send our bikes layer to the createMap function
   createMap(bikesLayer);
 }
 //==========================================================================================
 // Create the createMap function.
-function createMap(bikeStations) {
+function createMap(bikesLayer) {
 
   // Create the base layers.
-  var street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  var streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   })
 
-  var topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+  var topomap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
     attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
   });
 
@@ -57,14 +56,14 @@ function createMap(bikeStations) {
 
   // Create an overlay object to hold our overlay.
   var overlayMaps = {
-    "Bike Station": bikeStations
+    "Bike Station": bikesLayer
   };
 
   // Create our map, giving it the streetmap and bikeStations layers to display on load.
   var myMap = L.map("map-id", {
     center: [40.73, -74.0059],
     zoom: 12,
-    layers: [streetmap, topomap, bikeStations]
+    layers: [streetmap, topomap, bikesLayer]
   });
 
   // Create a layer control.
@@ -77,33 +76,4 @@ function createMap(bikeStations) {
 }
 
 
-  // Create the tile layer that will be the background of our map.
-
-
-  // Create a baseMaps object to hold the lightmap layer.
-
-
-  // Create an overlayMaps object to hold the bikeStations layer.
-
-
-  // Create the map object with options.
-
-
-  // Create a layer control, and pass it baseMaps and overlayMaps. Add the layer control to the map.
-
-// Create the createMarkers function.
-
-
-  // Pull the "stations" property from response.data.
-
-  // Initialize an array to hold the bike markers.
-
-  // Loop through the stations array.
-    // For each station, create a marker, and bind a popup with the station's name.
-
-    // Add the marker to the bikeMarkers array.
-
-  // Create a layer group that's made from the bike markers array, and pass it to the createMap function.
-
-
-// Perform an API call to the Citi Bike API to get the station information. Call createMarkers when it completes.
+  
